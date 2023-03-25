51dc85df473e0837a1f611586b30f046d3a8c30e
it20228644 <it20228644@my.sliit.lk>
2023-03-25 13:33:57 +0530

    app.js updated

--- ctse_assessment_01/App.js
+++ ctse_assessment_01/App.js
@@ -1,31 +1,67 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+/* import screen for navigation */
+import HomeScreen from './screens/UserList'
+import UserDetails from './screens/UserDetails'
+import CreateNewUser from './screens/CreateNewUser'
+import Homepage from './screens/Homepage';
+import MyScreen from './screens/MedicineHome';
+import AdverticingCardList from './screens/AdvertisingHome';
+import CreateAd from './screens/CreateNewAdvertising';
 import ChannelCenters from './screens/ChannelCenters';
 
-/* import screen for navigation */
+import SplashScreen from './components/SplashScreen';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
 
-import Homepage from './screens/Homepage';
-import MedicineManagementHome from './screens/MedicineHome';
 
 /* create stack for navigation */
 const Stack = createNativeStackNavigator()
 
 export default function App() {
+
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
+      
+
+      <Stack.Screen 
+          name='Splash' 
+          component={SplashScreen} 
+          options={{headerShown: false}}/>
       <Stack.Screen 
           name='Home' 
           component={Homepage} 
           options={{title: 'Welcome'}}/>
       <Stack.Screen 
-          name='Medicine' 
-          component={MedicineManagementHome} 
-          options={{title: 'Medicine Management'}}/>
-          <Stack.Screen 
           name='ChannelCenters Home' 
           component={ChannelCenters} 
           options={{title: 'Channel Centers Manage'}}/>
+      <Stack.Screen 
+          name='AdverticingCardList Home' 
+          component={AdverticingCardList} 
+          options={{title: 'Adverticing Manage'}}/>
+                <Stack.Screen 
+          name='CreateAd' 
+          component={CreateAd} 
+          options={{title: 'Create adverticing'}}/>
+      <Stack.Screen 
+          name='Medicne Home' 
+          component={MyScreen} 
+          options={{title: 'Medicne Management'}}/>
+
+        <Stack.Screen 
+          name='Users List' 
+          component={HomeScreen} 
+          options={{title: 'Users Management'}}/>
+        <Stack.Screen 
+          name='User Details'
+          component={UserDetails}/>
+        <Stack.Screen 
+          name='Create New User'
+          component={CreateNewUser}/>
+
       </Stack.Navigator>
     </NavigationContainer>
   );
