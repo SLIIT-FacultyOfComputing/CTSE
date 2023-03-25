d1d7578027b7dd1f93c5fe0bba136ffb992d46fb
RMRAmadushanka <it19147260@my.sliit.lk>
2023-03-25 11:15:19 +0530

    Add some Comments

--- ctse_assessment_01/screens/MedicineHome.js
+++ ctse_assessment_01/screens/MedicineHome.js
@@ -1,52 +1,53 @@
 import React, { useState, useEffect } from 'react';
 import { View, TouchableOpacity, Text, Modal, TextInput } from 'react-native';
-import CardList from '../components/CardList';
 import { ref, push, onValue } from "firebase/database";
+//
+import CardList from '../components/CardList';
 import { db } from '../config';
+
 const MyScreen = () => {
- const [data, setData] = useState([]);
-  const [showModal, setShowModal] = useState(false);
+  const [data, setData] = useState([]);
+  const [showMedicineModal, setShowMedicineModal] = useState(false);
   const [name, setName] = useState('');
   const [price, setPrice] = useState('');
   const [quantity, setQuantity] = useState('');
   const [imageUrl, setImageUrl] = useState('');
-
+  //
   const handleCardPress = (item) => {
     // Do something when a card is pressed
   };
-
+  //
   const handleAddMedicine = () => {
-    setShowModal(true);
+    setShowMedicineModal(true);
   };
-
-  const handleModalSubmit = async() => {
+  //Insert Medicine Function
+  const handleInsertMedicine = async () => {
     await push(ref(db, 'medicines'), {
       name,
       price,
       quantity,
       imageUrl
     }).then(() => {
-      setShowModal(false);
-  }).catch((error) => {
+      setShowMedicineModal(false);
+    }).catch((error) => {
       alert(error.message)
-  });
+    });
   };
-
+  //
   useEffect(() => {
     const usersRef = ref(db, "medicines");
-
     onValue(usersRef, (snapshot) => {
-        const medicines = [];
-        snapshot.forEach((childSnapshot) => {
-            const medicine = {
-                id: childSnapshot.key,
-                ...childSnapshot.val(),
-            };
-            medicines.push(medicine);
-        });
-        setData(medicines);
+      const medicines = [];
+      snapshot.forEach((childSnapshot) => {
+        const medicine = {
+          id: childSnapshot.key,
+          ...childSnapshot.val(),
+        };
+        medicines.push(medicine);
+      });
+      setData(medicines);
     });
-}, []);
+  }, []);
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -56,7 +57,7 @@ const MyScreen = () => {
         </TouchableOpacity>
       </View>
       <CardList data={data} onCardPress={handleCardPress} />
-      <Modal visible={showModal} animationType="slide">
+      <Modal visible={showMedicineModal} animationType="slide">
         <View style={styles.modalContent}>
           <TextInput
             placeholder="Name"
@@ -82,10 +83,10 @@ const MyScreen = () => {
             onChangeText={(text) => setImageUrl(text)}
             style={styles.textInput}
           />
-          <TouchableOpacity style={styles.addButton} onPress={handleModalSubmit}>
+          <TouchableOpacity style={styles.addButton} onPress={handleInsertMedicine}>
             <Text style={styles.addButtonText}>Add</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.cancelButton} onPress={() => setShowModal(false)}>
+          <TouchableOpacity style={styles.cancelButton} onPress={() => setShowMedicineModal(false)}>
             <Text style={styles.cancelButtonText}>Cancel</Text>
           </TouchableOpacity>
         </View>
@@ -93,73 +94,73 @@ const MyScreen = () => {
     </View>
   );
 };
-
+// styles for Medicine Home page
 const styles = {
-    container: {
-      flex: 1,
-      backgroundColor: 'white',
-    },
-    header: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      justifyContent: 'space-between',
-      paddingVertical: 10,
-      paddingHorizontal: 20,
-    },
-    headerText: {
-      fontSize: 20,
-      fontWeight: 'bold',
-    },
-    addItemButton: {
-      backgroundColor: 'blue',
-      width: 50,
-      height: 50,
-      borderRadius: 25,
-      alignItems: 'center',
-      justifyContent: 'center',
-      marginLeft: 10,
-    },
-    addItemButtonText: {
-      color: 'white',
-      fontSize: 24,
-      fontWeight: 'bold',
-    },
-    modalContent: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    textInput: {
-      borderWidth: 1,
-      borderColor: 'gray',
-      padding: 10,
-      marginBottom: 20,
-      width: '80%',
-    },
-    addButton: {
-      backgroundColor: 'blue',
-      paddingHorizontal: 20,
-      paddingVertical: 10,
-      borderRadius: 10,
-      marginTop: 10,
-    },
-    addButtonText: {
-      color: 'white',
-      fontSize: 18,
-      fontWeight: 'bold',
-    },
-    cancelButton: {
-      backgroundColor: 'gray',
-      paddingHorizontal: 20,
-      paddingVertical: 10,
-      borderRadius: 10,
-      marginTop: 10,
-    },
-    cancelButtonText: {
-      color: 'white',
-      fontSize: 18,
-      fontWeight: 'bold',
-    },
-  };
-  
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+  },
+  headerText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  addItemButton: {
+    backgroundColor: 'blue',
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 10,
+  },
+  addItemButtonText: {
+    color: 'white',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  modalContent: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 10,
+    marginBottom: 20,
+    width: '80%',
+  },
+  addButton: {
+    backgroundColor: 'blue',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 10,
+  },
+  addButtonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  cancelButton: {
+    backgroundColor: 'gray',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 10,
+  },
+  cancelButtonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+};
+//
 export default MyScreen;
